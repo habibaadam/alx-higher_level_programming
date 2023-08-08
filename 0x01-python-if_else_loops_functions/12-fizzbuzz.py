9f094640dc5c8d64e52aeaaca4ab18da4f68c7ae
habibaadam <habiepalmer@gmail.com>
2023-08-08 06:57:01 -0700

    12. Fizz Buzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    for value in range(1, 101):
+        if value % 3 == 0 and value % 5 == 0:
+            print("Fizzbuzz ", end="")
+        elif value % 3 == 0:
+            print("Fizz ", end="")
+        elif value % 5 == 0:
+            print("Buzz ", end="")
+        else:
+            print("{} ".format(value), end="")
