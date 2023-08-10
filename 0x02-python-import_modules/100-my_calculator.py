c1190a8741b7ff31a840b6cab175dcf7ac1593f2
habibaadam <habiepalmer@gmail.com>
2023-08-10 05:40:43 -0700

    6. Build my own calculator!

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+
+    args_num = len(sys.argv) - 1
+
+    """If the number of arguments is not 3, your program has to:
+print Usage: ./100-my_calculator.py <a> <operator> <b> followed with a new line
+exit with the value 1
+    """
+    if args_num != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    op = sys.argv[2]
+    if op != '+' and op != '-' and op != '*' and op != '/':
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    from calculator_1 import add, sub, mul, div
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+
+    if op == '+':
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif op == '-':
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif op == '*':
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    else:
+        print("{} / {} = {}".format(a, b, div(a, b)))
