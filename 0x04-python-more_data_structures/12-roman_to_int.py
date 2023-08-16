3d62fc2e0fb52c3efe718587e1b61d5bec735f03
habibaadam <habiepalmer@gmail.com>
2023-08-16 07:05:51 -0700

    12. Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,20 +5,19 @@ def roman_to_int(roman_string):
         return 0
 
     roman_val = {
-            'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
-        }
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
+    }
 
     res = 0
     old_val = 0
 
-    for character in roman_string:
+    for character in reversed(roman_string):
         val = roman_val.get(character, 0)
-        if val == 0:
-            return 0
-        if val > old_val:
-            res += val - 2 * old_val
-        else:
+        if val >= old_val:
             res += val
+        else:
+            res -= val
             old_val = val
 
         return res
