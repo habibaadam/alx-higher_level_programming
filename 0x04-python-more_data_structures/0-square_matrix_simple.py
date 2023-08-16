8218ce2cc77bd3f57e8529f36a2f17d939f6ef5d
habibaadam <habiepalmer@gmail.com>
2023-08-16 05:15:59 -0700

    0. Squared simple

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/sur/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    new_mat = matrix.copy()
+
+    for h in range(len(matrix)):
+        new_mat[h] = list(map(lambda x: x**2, matrix[h]))
+
+    return new_mat
