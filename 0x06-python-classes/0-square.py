35ee1e4ea8b1e9d66540bf4cd30ac64d6c17d0ee
habibaadam <habiepalmer@gmail.com>
2023-08-29 11:59:49 +0000

    0. My first square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """
-Defining a class called Square   
-"""   
-class Square:
-   """ Making it Empty
+Defining a class called Square
 """
-   pass
+
+
+class Square:
+    """Making it Empty """
+    pass
