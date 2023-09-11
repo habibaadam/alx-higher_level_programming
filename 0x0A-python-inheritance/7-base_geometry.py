462d8a31848942ea4f4c83869816ba7b223520ae
habibaadam <habiepalmer@gmail.com>
2023-09-11 15:43:20 +0000

    8. Rectangle

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-"""Inherit from baseGeometry"""
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""Defines a base geometry class BaseGeometry"""
 
 
-class Rectangle(BaseGeometry):
-    """a class to define rectangle using BaseGeometry"""
+class BaseGeometry:
+    """this class represents a base geometry"""
 
-    def __init__(self, width, height):
-        """Intialize a new Rectangle
+    def area(self):
+        """method not implemented yet"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates a value as an integer
         """
-        self.integer_validator("width", width)
-        self.__width = width
-        self.integer_validator("height", height)
-        self.__height = height
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """Defining class BaseGeometry"""
-BaseGeometry = __import__('7-base_geometry).BaseGeometry')
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """a class to define rectangle using BaseGeometry"""
