39a48f4857131487ed1a9fc7cd77ff6a34fe8491
habibaadam <habiepalmer@gmail.com>
2023-09-12 10:56:31 +0000

    0. Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defining a function that prints contents of a file"""
+
+
+def read_file(filename=""):
+    with open(filename, encoding="utf-8") as h:
+        print(h.read(), end="")
