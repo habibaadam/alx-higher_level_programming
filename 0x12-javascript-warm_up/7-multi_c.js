fc4a500acc15651fc794bf947cde109326984cf3
habibaadam <habiepalmer@gmail.com>
2023-11-13 11:30:41 +0000

    7. I love C

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,16 @@
+#!/usr/local/bin/node
+
+const arg = process.argv.slice(2);
+
+if (arg.length !== 1) {
+  console.log('Missing number of occurrences');
+} else {
+  const h = parseInt(arg[0]);
+  if (isNaN(h)) {
+    console.log('Missing number of occurrences');
+  } else {
+    for (let j = 0; j < h; j++) {
+      console.log('C is fun');
+    }
+  }
+}
