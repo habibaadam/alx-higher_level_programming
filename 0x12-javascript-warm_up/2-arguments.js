9221fa6ff3bf5aa4b0663b693c081213e187fca9
habibaadam <habiepalmer@gmail.com>
2023-11-13 10:20:05 +0000

    2. Arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+const length = process.argv.length - 2;
+if (length === 0) {
+  console.log('No argument');
+} else if (length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
