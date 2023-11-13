d0315750daed3811c81fd9a3c3224626eb841cb6
habibaadam <habiepalmer@gmail.com>
2023-11-13 13:56:59 +0000

    17. Increment object

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,16 @@
+#!/usr/local/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+
+console.log(myObject);
+myObject.incr = function () {
+  return myObject.value++;
+};
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
