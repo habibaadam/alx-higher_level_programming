7d66105523f29672320505c71edbf568bb2a8e19
habibaadam <habiepalmer@gmail.com>
2023-11-13 12:31:36 +0000

    6. Loop to languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const outputs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
-for (let h = 0; h < outputs.length; h++) {
+for (let h = 0; h < 3; h++) {
   console.log(outputs[h]);
 }
