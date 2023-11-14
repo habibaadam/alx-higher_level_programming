41f4fc3431d76faf0e9b850d44785522c9cc419d
habibaadam <habiepalmer@gmail.com>
2023-11-14 12:17:27 +0000

    8. Esrever

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+  const reversedlist = [];
+
+  for (let h = list.length - 1; h >= 0; h--) {
+    reversedlist.push(list[h]);
+  }
+  return reversedlist
+};
