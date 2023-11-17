b035a981484832043aef350328d15ea677558b84
habibaadam <habiepalmer@gmail.com>
2023-11-17 15:19:27 +0000

    5. All cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -6,22 +6,31 @@
 """
 
 import MySQLdb
-from sys import argv
+import sys
 
-if __name__ == '__main__':
-    db = MySQLdb.connect(host='localhost',
-                         port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
 
-    cursor = db.cursor()
+def main():
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        charset="utf8"
+    )
+    state_name = sys.argv[4]
+    cur = conn.cursor()
+    cur.execute("SELECT cities.name FROM cities \
+    JOIN states ON cities.state_id = states.id WHERE states.name LIKE %s \
+    ORDER BY cities.id", (state_name,))
+    row = cur.fetchall()
+    re = ""
+    for r in row:
+        re += r[0] + ", "
+    print(re[0:-2:])
+    cur.close()
+    conn.close()
 
-    cursor.execute("SELECT cities.id, cities.name FROM cities JOIN \
-            states ON cities.state_id = states.id WHERE \
-            states.name = %s ORDER BY cities.id ASC",
-                   (argv[4],))
 
-    cities = cursor.fetchall()
-    if cities is not None:
-        print(", ".join([h[1] for h in cities]))
-    cursor.close()
-    db.close()
+if __name__ == "__main__":
+    main()
